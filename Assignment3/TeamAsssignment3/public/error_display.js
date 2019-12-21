42ddd9a5e46d35e360e70ed354ba1c5f8e74163b
MissLavender <carmenlavender2017@gmail.com>
2019-12-20 16:06:52 -1000

    updated comments of error_display.js
    updated comments

--- Assignment3/TeamAsssignment3/public/error_display.js
+++ Assignment3/TeamAsssignment3/public/error_display.js
@@ -1,6 +1,6 @@
-$(function () {
-    let params = new URLSearchParams(window.location.search)
-    if (params.has('error')) {
-        alert(params.get('error').replace(',', '\n'))
+$(function () {//on page load
+    let params = new URLSearchParams(window.location.search)//create a URLSearchParams object of the window search
+    if (params.has('error')) {//if there is an error parameter in the search
+        alert(params.get('error').replace(',', '\n'))//alert the user to the error(s)
     }
 })
