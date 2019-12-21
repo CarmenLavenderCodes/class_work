e22e1f54cace1704e8d9722202bd0130e25f0910
MissLavender <carmenlavender2017@gmail.com>
2019-12-20 15:29:03 -1000

    update notes for cookie.js
    update notes for cookie.js

--- Assignment3/TeamAsssignment3/public/cookie.js
+++ Assignment3/TeamAsssignment3/public/cookie.js
@@ -1,5 +1,7 @@
-function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    return parts.length === 2 ? JSON.parse(decodeURIComponent(parts.pop().split(";").shift())) : null
+//https://stackoverflow.com/questions/10730362/get-cookie-by-name we used this function 
+
+function getCookie(name) {//get cookie from document by its name
+    var value = "; " + document.cookie;// add ";" to make it easier to parse 
+    var parts = value.split("; " + name + "=");//splits cookie by cookie name
+    return parts.length === 2 ? JSON.parse(decodeURIComponent(parts.pop().split(";").shift())) : null//If it found the cookie, decode, parse and return it, otherwise return null
 }
